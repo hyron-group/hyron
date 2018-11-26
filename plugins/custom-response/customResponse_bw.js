30bc73bfe8d5c1da221aed110d9219fee7282532
thangdjw <thangphung.work@gmail.com>
2018-11-27 04:55:09 +0700

    fix bug

--- plugins/custom-response/customResponse_bw.js
+++ plugins/custom-response/customResponse_bw.js
@@ -3,7 +3,6 @@ const handleMapping = require("./responseMapping");
 module.exports = async function(req, res, prev) {
     prev = await prev;
     if (typeof prev == "object" && !(prev instanceof Array)) {
-        console.log("custom");
         Object.keys(prev).forEach(field => {
             var handle = handleMapping[field];
             if (handle != null) handle(prev[field], res);
