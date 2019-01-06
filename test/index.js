77ecf45c38f4e7413cadbece19e83494aa53458d
Spatocode <impulseofficial069@gmail.com>
2019-01-06 09:44:51 +0100

    [test] hyron instance test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,19 @@
-require('./benchmark/raw-node');
+/**require('./benchmark/raw-node');
 // require('./benchmark/express');
 require('./benchmark/hyron')
+*/
+
+var assert = require('assert')
+var hyron = require('../')
+
+describe('hyron', function(){
+    it('should register an instance without address', function(){
+        var app = hyron.getInstance()
+        assert.strictEqual(typeof app, 'object')
+    })
+
+    it('should register an instance with address', function(){
+        var app = hyron.getInstance(3000, 'localhost')
+        assert.strictEqual(typeof app, 'object')
+    })
+})
