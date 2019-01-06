ceac7e3dca5b546334936ada76fd08b318035303
Spatocode <impulseofficial069@gmail.com>
2019-01-06 19:42:54 +0100

    stub logger to improve readability in test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,22 @@
-/**require('./benchmark/raw-node');
-// require('./benchmark/express');
-require('./benchmark/hyron')
-*/
-
 var assert = require('assert')
 var hyron = require('../')
+var logger = require('../lib/logger')
+var sinon = require('sinon')
+var proxyquire = require('proxyquire')
+
+proxyquire = proxyquire.noCallThru().noPreserveCache()
 
 describe('hyron', function(){
+    beforeEach(function(){
+        this.stubInfo = sinon.stub(logger, 'info')
+        this.stubError = sinon.stub(logger, 'error')
+        this.stubWarn = sinon.stub(logger, 'warn')
+    })
+    afterEach(function(){
+        this.stubInfo.restore()
+        this.stubError.restore()
+        this.stubWarn.restore()
+    })
     it('should register an instance without address', function(){
         var app = hyron.getInstance()
         assert.strictEqual(typeof app, 'object')
