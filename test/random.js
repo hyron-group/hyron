d76d1be7b89a17d6b5a1dc5ccd6c386f710eb83f
thangdjw <thangphung.work@gmail.com>
2019-02-12 11:19:40 +0700

    update

--- test/random.js
+++ test/random.js
@@ -6,7 +6,7 @@ dynamicURL.registerUrl('/users/:uid/class/:classId');
 dynamicURL.registerUrl('/users/:uid/room/:roomId');
 dynamicURL.registerUrl('/users/:uid/argz/:orgz/depart/:name');
 
-
+var eventName = dynamicURL.getEventName('/users/thangdjw/class/32');
+console.log(eventName);
 var param = dynamicURL.getParams('/users/thangdjw/class/32');
-
 console.log(param)
