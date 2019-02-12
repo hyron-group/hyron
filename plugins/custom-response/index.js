be61edb3182f8de45361b98bf3e3dd4184d3ec16
thangdjw <thangphung.work@gmail.com>
2019-02-12 22:21:38 +0700

    update

--- plugins/custom-response/index.js
+++ plugins/custom-response/index.js
@@ -1,14 +1,3 @@
-
-/**
- * This backware used to allow you can todo more with response, that http supported.
- * You can used some type of http response inside main handle, just return as a object :
- * $type : set content type for response data
- * $data : is response data
- * $status : status code
- * $message : status message
- * $headers : set header data
- * $redirect : redirect to a page
- */
 module.exports = {
     backware : require("./customResponse_bw")
 }
